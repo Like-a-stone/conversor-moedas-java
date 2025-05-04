184781bd679d3f69953349a9f67e8d3da6bc8501
Lucas <lucassaox@gmail.com>
2025-05-04 02:02:51 -0300

    ✨ feat(client): adiciona ClientAPI para consultar taxas na Exchangerate-api

--- src/main/java/com/github/lucas/client/ClientAPI.java
+++ src/main/java/com/github/lucas/client/ClientAPI.java
@@ -1,9 +1,37 @@
 package com.github.lucas.client;
 
-import io.github.cdimascio.dotenv.Dotenv;
+import java.io.IOException;
+import java.math.BigDecimal;
+import java.net.URI;
+import java.net.http.HttpClient;
+import java.net.http.HttpRequest;
+import java.net.http.HttpResponse;
+
+import static com.github.lucas.config.EnvConfig.getApiKey;
 
 public class ClientAPI {
-    private final Dotenv dotenv = Dotenv.configure()
-            .load();
+    private final HttpClient client = HttpClient.newHttpClient();
 
+    public void pairConversion (String base, String target, BigDecimal amount){
+        String urlStr = settingUrl (base, target, amount);
+
+        try {
+            HttpRequest request = HttpRequest.newBuilder()
+                    .uri(URI.create(urlStr))
+                    .build();
+
+            HttpResponse<String> response = client
+                    .send(request, HttpResponse.BodyHandlers.ofString());
+            System.out.println(response.body());
+
+        }catch (IOException | InterruptedException e) {
+            System.out.println("Erro ao consultar o endereço: "+ e.getMessage());
+        }
+    }
+
+    private String settingUrl (String base, String target, BigDecimal amount){
+        String API_KEY = getApiKey();
+        return "https://v6.exchangerate-api.com/v6/%s/pair/%s/%s/%s"
+                .formatted(API_KEY, base , target, amount);
+    }
 }
