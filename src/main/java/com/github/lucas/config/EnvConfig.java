b59da0bf2cf2c5937af14ec2e34dafaaa639b7b9
Lucas <lucassaox@gmail.com>
2025-05-04 01:58:58 -0300

    ✨ feat(config): adiciona EnvConfig para acesso centralizado à API_KEY

---
+++ src/main/java/com/github/lucas/config/EnvConfig.java
@@ -0,0 +1,13 @@
+package com.github.lucas.config;
+
+
+import io.github.cdimascio.dotenv.Dotenv;
+
+public class EnvConfig {
+    private static final Dotenv dotenv = Dotenv.configure().load();
+
+    public static String getApiKey() {
+        return dotenv.get("API_KEY");
+    }
+}
+
